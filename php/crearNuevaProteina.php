ef7bdf0a01322d2548cc57da39990a72613a995d
spena21 <91271289+spena21@users.noreply.github.com>
2023-01-12 16:07:41 +0100

    Update crearNuevaProteina.php

--- php/crearNuevaProteina.php
+++ php/crearNuevaProteina.php
@@ -26,21 +26,25 @@
     $idProteina=$_POST["idProteina"];
     
     $sql= "Insert into proteinas Values(".$nom.", ".$resolució.", ".$metode.", "$imatge.", ";
-
-//  'jacinto';
-//$resolucio =$_POST ["resolucio"];//  '1234';
-//$especie =$_POST ["especie"];
-//$data =$_POST ["data"];
-//$metode=$_POST ["metode"];
-/*$idProteina=$_POST ["idProteina"];
+    $res = move_uploaded_file($_FILES["imatge"]["tmp_name"], "../img/farmacos/".$imatge);
 
 $sql= "SELECT * FROM proteinas where nombre = '$nom' AND idProteina = '$idProteina'";*/
 $resultado = mysqli_query($conexion, $sql);
 //header("Location: ../proteinas.html?resultado=".$resultado);
 if (mysqli_num_rows($resultado) > 0) {
+    echo'<form id="form" style="margin-left:0px;" method="post">
+    <input type="text" value="true" />
+    <input type="text" class="search-form" placeholder="Resolució" />
+    <input type="text" class="search-form" placeholder="Especie" />
+     <input type="text" class="search-form" placeholder="Metode" name="metode"/>
+     <input type="text" class="search-form" placeholder="Descripció" name="descripcio"/>
+    <input type="file" class="search-form" placeholder="Image" name="imatge" style="margin-top:22px; margin-left:20px; " />
+    <input type="submit" class="search-button" value="Guardar" />
+</form>';
+
 //mostrar la pàgina de inicio con el usuario logado
 while($row = mysqli_fetch_assoc($resultado)) {
-    $datos = $datos.
+    //$datos = $datos.
     "<div class='first-body'><img class='body-images' src='".$row["imagen"]."'/><div class='inner-first-body'><h1><a href='proteina.html'>".$row["nombre"]."</a></h1><p>
                 ".$row["descripcion"]."</p></div></div>";
     //<h1>id: " . $row["idProteina"]. " - Nom: " . $row["nombre"]. " - Resolucio" . $row["resolucion"]."</h1>";
