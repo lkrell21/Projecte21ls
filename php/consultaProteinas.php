89454fbc44d087ac7d6cc96c898e55b40b8aa32c
lkrell21 <lkrell21@ilg.cat>
2023-02-23 18:33:34 +0100

    php

--- php/consultaProteinas.php
+++ php/consultaProteinas.php
@@ -1,73 +1,76 @@
-<?php include_once('conexionBBDD.php');?>
+<?php include_once('conexionBBDD.php'); ?>
 <html>
- 
-<head> 
-<meta charset="UTF-8">
+
+<head>
+    <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <link rel="stylesheet" href="../css/farmacos.css">
     <link rel="stylesheet" href="../css/general.css">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">  
-    <title>PROTEINAS</title></head>
+    <link rel="stylesheet" href="../css/proteinas.css"> 
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <title>PROTEINAS</title>
+</head>
+
 <body> <!--<h1>Consultar</h1>-->
-<header style="background-image: url('img/header3.jpg'); height: 200px;">
-<h1 id="header">Lucas & Sheila webproject</h1>
-<nav style ="margin-top: 55px;">
-    <a href="home.html">Home</a>
-    <a href="estadisticas.html">Estadisticas</a>
-    <a href="farmacos.html">Farmacos</a>
-    <a href="proteinas.html">Proteinas</a>
-    <a href="#">Users</a>
+    <header style="background-image: url('../img/header3.jpg'); height: 200px;">
+        <h1 id="header">Lucas & Sheila webproject</h1>
+        <nav style="margin-top: 55px;">
+            <a href="../home.html">Home</a>
+            <a href="../estadisticas.html">Estadísticas</a>
+            <a href="../farmacos.html">Fármacos</a>
+            <a href="../proteinas.html">Proteínas</a>
+            <a href="listaUsers.php">Users</a>
 
-</nav>
-<a href="login.html">
-    <button type="submit">Login</button>
-</a>
-</header>
+        </nav>
+        <a href="../login.html">
+            <button type="submit">Login</button>
+        </a>
+    </header>
 
-<div id="body">
-    <div class="search-body">
-        <div id="form-div">
-            <form id="form" action="consultaProteinas.php" method="post">
-                <input type="text" class="search-form" placeholder="Nom" name="nom"/>
-                <input type="text" class="search-form" placeholder="Resolució" name="resolucio"/>
-                <input type="text" class="search-form" placeholder="Especie" name="especie"/>
-                <input type="text" class="search-form" placeholder="Data" name="data" />
-                <input type="text" class="search-form" placeholder="Metode" name="metode"/>
-                <input type="text" class="search-form" placeholder="Codi de la proteïna" name="idProteina"/>
-                <input type="submit" class="search-button" value="Cerca" onclick='busqueda()' />
-            </form>
+    <div id="body">
+        <div class="search-body">
+            <div id="form-div">
+                <form id="form" action="consultaProteinas.php" method="post">
+                    <input type="text" class="search-form" placeholder="Nom" name="nom" />
+                    <input type="text" class="search-form" placeholder="Resolució" name="resolucio" />
+                    <input type="text" class="search-form" placeholder="Especie" name="especie" />
+                    <input type="text" class="search-form" placeholder="Data" name="data" />
+                    <input type="text" class="search-form" placeholder="Metode" name="metode" />
+                    <input type="text" class="search-form" placeholder="Codi de la proteïna" name="idProteina" />
+                    <input type="submit" class="search-button" value="Cerca" onclick='busqueda()' />
+                </form>
+            </div>
         </div>
-    </div>
-<?php
+        <?php
 
-$sql = "SELECT * FROM proteinas";
-//coger usuario y contraseña del formulario del login
-if($_POST["nom"] != null){
-    $nom=$_POST["nom"];
-    $sql= $sql." where nombre = '$nom'";
-}
-if($_POST["resolucio"] != null){
-    $resolucio=$_POST["resolucio"];
-    $sql= $sql." AND resolucion = '$resolucio'";
-}
-if($_POST["especie"] != null){
-    $especie=$_POST["especie"];
-    $sql= $sql." AND especie = '$especie'";
-}
-if($_POST["data"] != null){
-    $fecha=$_POST["data"];
-    $sql= $sql." AND cast(date,fecha) = '$fecha'";
-}
-if($_POST["metode"] != null){
-    $fecha=$_POST["metode"];
-    $sql= $sql." AND metode = '$metode'";
-}
-if($_POST["idProteina"] != null){
-    $idProteina=$_POST["idProteina"];
-    $sql= $sql." AND idProteina = '$idProteina'";
-}
-$datos = "";
-//  'jacinto';
+        $sql = "SELECT * FROM proteinas";
+        //coger usuario y contraseña del formulario del login
+        if ($_POST["nom"] != null) {
+            $nom = $_POST["nom"];
+            $sql = $sql . " where nombre = '$nom'";
+        }
+        if ($_POST["resolucio"] != null) {
+            $resolucio = $_POST["resolucio"];
+            $sql = $sql . " AND resolucion = '$resolucio'";
+        }
+        if ($_POST["especie"] != null) {
+            $especie = $_POST["especie"];
+            $sql = $sql . " AND especie = '$especie'";
+        }
+        if ($_POST["data"] != null) {
+            $fecha = $_POST["data"];
+            $sql = $sql . " AND cast(date,fecha) = '$fecha'";
+        }
+        if ($_POST["metode"] != null) {
+            $fecha = $_POST["metode"];
+            $sql = $sql . " AND metode = '$metode'";
+        }
+        if ($_POST["idProteina"] != null) {
+            $idProteina = $_POST["idProteina"];
+            $sql = $sql . " AND idProteina = '$idProteina'";
+        }
+        $datos = "";
+        //  'jacinto';
 //$resolucio =$_POST ["resolucio"];//  '1234';
 //$especie =$_POST ["especie"];
 //$data =$_POST ["data"];
@@ -75,19 +78,22 @@ $datos = "";
 /*$idProteina=$_POST ["idProteina"];
 
 $sql= "SELECT * FROM proteinas where nombre = '$nom' AND idProteina = '$idProteina'";*/
-$resultado = mysqli_query($conexion, $sql);
-//header("Location: ../proteinas.html?resultado=".$resultado);
-if (mysqli_num_rows($resultado) > 0) {
-//mostrar la pàgina de inicio con el usuario logado
-while($row = mysqli_fetch_assoc($resultado)) {
-    $datos = $datos.
-    "<div class='first-body'><img class='body-images' src='".$row["imagen"]."'/><div class='inner-first-body'><h1><a href='proteina.html'>".$row["nombre"]."</a></h1><p>
-                ".$row["descripcion"]."</p></div></div>";
-    //<h1>id: " . $row["idProteina"]. " - Nom: " . $row["nombre"]. " - Resolucio" . $row["resolucion"]."</h1>";
-}
-echo $datos;
-} else echo "Usuario o contraseña incorrecto.";
+        $resultado = mysqli_query($conexion, $sql);
+        //header("Location: ../proteinas.html?resultado=".$resultado);
+        if (mysqli_num_rows($resultado) > 0) {
+            //mostrar la pàgina de inicio con el usuario logado
+            while ($row = mysqli_fetch_assoc($resultado)) {
+                $datos = $datos .
+                    "<div class='first-body'><img class='body-images' src='" . $row["imagen"] . "'/><div class='inner-first-body'><h1><a href='proteina.html'>" . $row["nombre"] . "</a></h1><p>
+                " . $row["descripcion"] . "</p></div></div>";
+                //<h1>id: " . $row["idProteina"]. " - Nom: " . $row["nombre"]. " - Resolucio" . $row["resolucion"]."</h1>";
+            }
+            echo $datos;
+        } else
+            echo "Usuario o contraseña incorrecto.";
 
-//header("Location: ../proteinas.html?resultado=".$datos);
-?>
-</body></html>
+        //header("Location: ../proteinas.html?resultado=".$datos);
+        ?>
+</body>
+
+</html>
--- php/listaUsers.php
+++ php/listaUsers.php
@@ -1,6 +1,7 @@
-<?php include_once('conexionBBDD.php');?>
+<?php include_once('conexionBBDD.php'); ?>
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -10,38 +11,45 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <title>paginaweb</title>
 </head>
-<body>
-    <header style="background-image: url('img/header3.jpg'); height: 200px;">
-        <h1 id="header">Lucas & Sheila webproject</h1>
-    <nav style ="margin-top: 55px;">
-        <a href="../home.html">Home</a>
-        <a href="../estadisticas.html">Estadísticas</a>
-        <a href="../farmacos.html">Fármacos</a>
-        <a href="../proteinas.html">Proteínas</a>
-        <a href="/listaUsers">Users</a>
 
-    </nav>
-    <a href="login.html">
-        <button id="btnLogin">Login</button>
-    </a>
+<body>
+    <header style="background-image: url('../img/header3.jpg'); height: 200px;">
+        <h1 id="header">Lucas & Sheila webproject</h1>
+        <nav style="margin-top: 55px;">
+            <a href="../home.html">Home</a>
+            <a href="../estadisticas.html">Estadísticas</a>
+            <a href="../farmacos.html">Fármacos</a>
+            <a href="../proteinas.html">Proteínas</a>
+            <a href="listaUsers.php">Users</a>
+
+        </nav>
+        <a href="../login.html">
+            <button id="btnLogin">Login</button>
+        </a>
     </header>
-    
+
     <div id="body">
-      <table>
-        <tr>
-          <th>ID</th>
-          <th>Nombre</th>
-          <th>Correo electrónico</th>
-          <?php $sql = "SELECT * FROM usuarios";
-          $result = mysqli_query($conexion, $sql);
-          while($row = mysqli_fetch_assoc($result)): ?>
-          <tr>
-            <td><?php echo $row['id']; ?></td>
-            <td><?php echo $row['username']; ?></td>
-            <td><?php echo $row['email']; ?></td>
-          </tr>
-          <?php endwhile; ?>
-      </table>
+        <table>
+            <tr>
+                <th>ID</th>
+                <th>Nombre</th>
+                <th>Correo electrónico</th>
+                <?php $sql = "SELECT * FROM usuarios";
+                $result = mysqli_query($conexion, $sql);
+                while ($row = mysqli_fetch_assoc($result)): ?>
+                <tr>
+                    <td>
+                        <?php echo $row['idUsuario']; ?>
+                    </td>
+                    <td>
+                        <?php echo $row['nombre']; ?>
+                    </td>
+                    <td>
+                        <?php echo $row['email']; ?>
+                    </td>
+                </tr>
+            <?php endwhile; ?>
+        </table>
     </div>
 
     <footer class="footer">
@@ -49,10 +57,11 @@
         <div class="footer-left">
             <h3>L&S<span>WebProject</span></h3>
             <p class="footer-links">
-                <a href="home.html">Home</a>
-                <a href="estadisticas.html">Estadísticas</a>
-                <a href="farmacos.html">Fármacos</a>
-                <a href="proteinas.html">Proteínas</a>
+            <a href="../home.html">Home</a>
+            <a href="../estadisticas.html">Estadísticas</a>
+            <a href="../farmacos.html">Fármacos</a>
+            <a href="../proteinas.html">Proteínas</a>
+            <a href="listaUsers.php">Users</a>
             </p>
         </div>
 
@@ -87,4 +96,5 @@
 
     </footer>
 </body>
+
 </html>
